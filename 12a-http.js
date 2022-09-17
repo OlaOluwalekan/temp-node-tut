3c52228d8c13f3f22d50ca1534728830c74b352b
OlaOluwalekan <olalekanbello534@gmail.com>
2022-09-17 10:03:31 +0100

    some modification

--- 12a-http.js
+++ 12a-http.js
@@ -3,7 +3,7 @@ const http = require("http");
 http
   .createServer((req, res) => {
     if (req.url === "/") {
-      res.write(`<h1>Home</h1> <a href="/about">About Us</a>`);
+      res.write(`<h1>Home Page</h1> <a href="/about">About Us</a>`);
       res.end();
     } else if (req.url === "/about") {
       res.write(`<h1>About</h1> <a href="/">Home</a>`);
